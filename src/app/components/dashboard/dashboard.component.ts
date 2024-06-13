b18500bd9925cdbcd7635c3ed529f895b59851c7
GS-Charan <gs.charan1605@gmail.com>
2024-06-14 01:24:32 +0530

    final working changes of today fixed the null value

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { DataService } from '../../data.service';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { TokenStorageService } from '../../token-storage.service';
+import { Router } from '@angular/router'; 
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+	
+  jsondata = {
+    name: '5',
+    time: ''
+    
+  };
+  title = 'social_new';
+  response: any = {};
+    isLoggedIn = true;
 
+  
+   
+  constructor(private dataService: DataService,private tokenStorageService: TokenStorageService,private router: Router) {}
+  ngOnInit() {
+    
+  }
+  onSubmit() {
+	  const data = {
+      name: this.jsondata.name,
+      time: this.jsondata.time
+    };
+	  
+	  console.log("before submit"+data);
+	  
+    this.dataService.sendData(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          
+        },
+        error: (e) => console.error(e)
+      });
+      
+     this.dataService.recieveData()
+         .subscribe({
+			next: (res) => {
+          console.log(res);
+          this.response = res;
+          
+        }, error: (e) => console.error(e)
+			 
+		 });
+      
+  }
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);  
+  }
+  logout(): void {
+    this.tokenStorageService.signOut();
+            this.navigateToLogin();
+
+  }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,32 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';	
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
+
 @Injectable({
   providedIn: 'root'
 })
-
 export class DataService {
+  private authToken: string | null = null;
+  private httpOptionsAuth: { headers: HttpHeaders } | null = null;
 
-  constructor(private http: HttpClient) {
-	  
-   }
-   sendData(data:any):Observable<any>
-   {
-	   return this.http.post('http://localhost:8080/api/name/send',  data,{responseType: 'json'});
-	   
-   }
-   
-   recieveData():Observable<any>
-   {
-	   return this.http.get('http://localhost:8080/api/names/fetchAll');
-	   
-   }
-   
-     login(credentials): Observable<any> {
+  constructor(private http: HttpClient, private tokenStorageService: TokenStorageService) {
+    this.initializeService();
+  }
+
+  private initializeService() {
+    this.authToken = this.tokenStorageService.getToken();
+    this.httpOptionsAuth = this.authToken
+      ? {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.authToken}`
+          })
+        }
+      : null;
+  }
+
+  sendData(data: any): Observable<any> {
+    return this.http.post('http://localhost:8080/api/name/send', data, this.httpOptionsAuth);
+  }
+
+  recieveData(): Observable<any> {
+    return this.http.get('http://localhost:8080/api/names/fetchAll',this.httpOptionsAuth);
+  }
+
+  login(credentials): Observable<any> {
     return this.http.post('http://localhost:8080/api/auth/signin', {
       username: credentials.username,
       password: credentials.password
